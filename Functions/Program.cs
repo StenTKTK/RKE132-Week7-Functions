4222df53b2e777f5316fd7cf76435c394f2b63e7
Sten Eric Persidski <sten.eric.persidski@tktk.ee>
2024-12-19 12:31:57 +0200

    add week7 code

---
+++ Functions/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Are you coming or leaving? (in/out):");
+string userChoice = Console.ReadLine();
+
+if(userChoice == "in")
+{
+    Printhello();
+}
+else
+{
+    PrintGoodBye();
+}
+
+
+
+static void Printhello() //function /method
+{
+    Console.WriteLine("Hello, world!");
+}
+
+static void PrintGoodBye()
+{
+    Console.WriteLine("See you later, aligator.");
+}
